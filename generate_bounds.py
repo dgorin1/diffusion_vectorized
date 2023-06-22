90ac1a86a87f17bcd4fd9296715266adde6c2cfa
dgorin1 <andrew_gorin@berkeley.edu>
2023-06-22 13:03:01 -0700

    updated mineral list to include kspar

--- generate_bounds.py
+++ generate_bounds.py
@@ -1,7 +1,7 @@
 def generate_bounds(ndom:int, moles_bound, mineral_name:str,stat = "chisq"):
 
 
-    if mineral_name == "quartz" or mineral_name == "Quartz":
+    if mineral_name.lower() == "quartz":
         if stat.lower() == "chisq" or stat.lower() == "l2_moles" or stat.lower() == "l1_moles":
             moles = True
         else:
@@ -21,5 +21,25 @@ def generate_bounds(ndom:int, moles_bound, mineral_name:str,stat = "chisq"):
                 return [moles_bound,Ea_bounds]+ ndom*[lnd0aa_bounds]+ (ndom-1)*[frac_bounds]
             else:
                 return [Ea_bounds]+ ndom*[lnd0aa_bounds]+ (ndom-1)*[frac_bounds]
+    
+    elif mineral_name == "kspar":
+        if stat.lower() == "chisq" or stat.lower() == "l2_moles" or stat.lower() == "l1_moles":
+            moles = True
+        else:
+            moles = False
 
+        Ea_bounds = (100,400)
+        lnd0aa_bounds = (-50,50)
+        frac_bounds = (0,1)
+
+        if ndom == 1:
+            if moles == True:
+                return [moles_bound,Ea_bounds,lnd0aa_bounds]
+            else:
+                return [Ea_bounds,lnd0aa_bounds]
+        elif ndom >1:
+            if moles == True:
+                return [moles_bound,Ea_bounds]+ ndom*[lnd0aa_bounds]+ (ndom-1)*[frac_bounds]
+            else:
+                return [Ea_bounds]+ ndom*[lnd0aa_bounds]+ (ndom-1)*[frac_bounds]
 
