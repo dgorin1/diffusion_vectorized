ff90de0bd4151d47b92cdea55a8b5a1592fa28a5
dgorin1 <andrew_gorin@berkeley.edu>
2023-08-17 12:42:35 -0700

    Added pyroxene option for diffusion kinetics

--- generate_inputs.py
+++ generate_inputs.py
@@ -31,7 +31,7 @@ def generate_inputs(nameOfInputCSVFile, nameOfExperimentalResultsFile,geometry:s
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-nameOfInputCSVFile =  f"{dir_path}/data/3DomSynthDataNoisyM3_plane_sheet_noextrasteps.csv"
-nameOfExperimentalResultsFile = f"{dir_path}/data/input_3DomSynthDataNoisyM3_plane_sheet_noextrasteps.csv"
-geometry = "plane sheet"
+nameOfInputCSVFile =  f"{dir_path}/data/LABCO-Px-Dc.csv"
+nameOfExperimentalResultsFile = f"{dir_path}/data/input_LABCO-Px-Dc.csv"
+geometry = "spherical"
 generate_inputs(nameOfInputCSVFile, nameOfExperimentalResultsFile,geometry)
