77b98cc82f7f5b1c07529b194187f1d8e3909609
dgorin1 <andrew_gorin@berkeley.edu>
2023-08-23 18:24:49 -0700

    use

--- tester_vectorized.py
+++ tester_vectorized.py
@@ -20,11 +20,11 @@ from save_results import save_results
 
 # get this file's directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
-data_input = pd.read_csv(f"{dir_path}/data/input_KM95-6-Db-1100um.csv")
+data_input = pd.read_csv(f"{dir_path}/data/input_KM95-20-Da-665um.csv")
 mineral_name = "quartz"
-time_add = [3600*5,114048000] #Add extra time in seconds
+time_add = [3600*5,86918400] #Add extra time in seconds
 temp_add = [40,21.111111111]
-sample_name = "KM95-6-Db-1100um-nolookuptable"
+sample_name = "KM95-20-Da-665_REDO_nolookuptable"
 moves = "quartz" # Define moves as "snooker" if you fear multimodality in your dataset. Can lead to poor performance if no multimodality exists
 max_domains_to_model = 10
 geometry  = "spherical" #"plane sheet" # options are "plane sheet", or "spherical"
