805f65a9e7c8ebdbcea8b546c9b56789278100c2
dgorin1 <andrew_gorin@berkeley.edu>
2023-08-27 14:18:13 -0700

    use

--- tester_vectorized.py
+++ tester_vectorized.py
@@ -20,12 +20,12 @@ from save_results import save_results
 
 # get this file's directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
-data_input = pd.read_csv(f"{dir_path}/data/input_KM95-24-a-Da-1130um.csv")
-mineral_name = "quartz"
-time_add = [3600*5,102124800] #Add extra time in seconds
+data_input = pd.read_csv(f"{dir_path}/data/input_LABCO-Plag-Dd_spherical.csv")
+mineral_name = "plag"
+time_add = [3600*5,8*365.25*24*3600] #Add extra time in seconds
 temp_add = [40,21.111111111]
-sample_name = "KM95-24-Da-1130um_REDO_nolookuptable"
-moves = "quartz" # Define moves as "snooker" if you fear multimodality in your dataset. Can lead to poor performance if no multimodality exists
+sample_name = "LABCO-Plag-Dd"
+moves = "snooker" # Define moves as "snooker" if you fear multimodality in your dataset. Can lead to poor performance if no multimodality exists
 max_domains_to_model = 10
 geometry  = "spherical" #"plane sheet" # options are "plane sheet", or "spherical"
 omit_value_indices = []#[33,34,35,36,37,38,39,40,41]
